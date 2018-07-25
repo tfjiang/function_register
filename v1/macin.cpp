b05a398340ca9ec2e27e3c8a1841abc254afef3b
tfjiang <jtf198711@163.com>
2018-07-26 00:33:52 +0800

    modify v1

--- v1/macin.cpp
+++ v1/macin.cpp
@@ -1,5 +1,6 @@
 #include "function_register.h"
 #include <iostream>
+#include <string>
 
 void plus(const std::any&data)
 {
@@ -14,21 +15,34 @@ void plus_100(const std::any&data)
 	std::cout << a << "+" << 100 << "=" << a + 100 << std::endl;
 }
 
+class test_output
+{
+public:
+	void ppp(const std::any &a)
+	{
+		std::cout << std::any_cast<const std::string>(a) << std::endl;
+	}
+};
+
+
 int main()
 {
 	function_register fr;
+	test_output to;
 	fr.regist("plus", std::bind(&plus, std::placeholders::_1));
 	fr.regist("plus_100", std::bind(&plus_100, std::placeholders::_1));
 	fr.regist("plus_triple", [](const std::any& triple)
 	{
-		int a,b;
+		int a, b;
 		double c;
 		std::tie(a, b, c) = std::any_cast<std::tuple<int, int, double>>(triple);
 		std::cout << a << "+" << b << "+" << c << "=" << a + b + c << std::endl;
 	});
+	fr.regist("test_output::ppp", std::bind(&test_output::ppp, &to, std::placeholders::_1));
 
 	fr.run("plus", std::make_pair(1, 2));
 	fr.run("plus_100", 1000);
 	fr.run("plus_triple", std::make_tuple(1,10,300.1));
+	fr.run("test_output::ppp", std::string("Hello World!"));
 	return 0;
 }
